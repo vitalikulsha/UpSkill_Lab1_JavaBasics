862e70b4913b8b40423e9a8dd9a614e5d4de2dc8
vitalikulsha <kulsha_85@mail.ru>
2021-03-31 19:35:55 +0300

    update test and pom

--- JUnit/src/test/java/QuadraticEquationTest.java
+++ JUnit/src/test/java/QuadraticEquationTest.java
@@ -1,46 +1,29 @@
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class QuadraticEquationTest {
-    QuadraticEquation quadraticEquation;
-
-    @Before
-    public void setQuadraticEquation() {
-        this.quadraticEquation = new QuadraticEquation();
-    }
 
     @Test
     public void calculate_numberAIsZero() {
-        quadraticEquation.setNumberA(0);
-        quadraticEquation.setNumberB(4);
-        quadraticEquation.setNumberC(1);
+        QuadraticEquation quadraticEquation = new QuadraticEquation(0, 4, 1);
         Assert.assertNull(quadraticEquation.calculate());
     }
 
     @Test
     public void calculate_discriminantNegative() {
-        quadraticEquation.setNumberA(5);
-        quadraticEquation.setNumberB(1);
-        quadraticEquation.setNumberC(1);
+        QuadraticEquation quadraticEquation = new QuadraticEquation(5, 1, 1);
         Assert.assertNull(quadraticEquation.calculate());
     }
 
     @Test
     public void calculate_discriminantZero() {
-        quadraticEquation.setNumberA(2);
-        quadraticEquation.setNumberB(4);
-        quadraticEquation.setNumberC(2);
+        QuadraticEquation quadraticEquation = new QuadraticEquation(2, 4, 2);
         Assert.assertArrayEquals(new double[]{-1}, quadraticEquation.calculate(), 0);
     }
 
     @Test
     public void calculate_discriminantPositive() {
-        quadraticEquation.setNumberA(2);
-        quadraticEquation.setNumberB(4);
-        quadraticEquation.setNumberC(0);
+        QuadraticEquation quadraticEquation = new QuadraticEquation(2, 4, 0);
         Assert.assertArrayEquals(new double[]{0, -2}, quadraticEquation.calculate(), 0);
     }
 }
