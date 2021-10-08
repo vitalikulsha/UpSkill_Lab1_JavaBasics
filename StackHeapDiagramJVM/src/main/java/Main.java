4962d6a6472b96d86de0dbeb43e3788eb6cc8540
vitalikulsha <kulsha_85@mail.ru>
2021-10-08 19:03:32 +0300

    initial commit

---
+++ StackHeapDiagramJVM/src/main/java/Main.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import org.apache.log4j.Logger;
+
+public class Main {
+    private final static Logger LOG = Logger.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        LOG.debug("Main start");
+        String name = "Kevin";
+        List<String> list = new ArrayList<>();
+        int times = 10;
+
+        System.out.println(times + fill(list, name + name, times));
+    }
+
+    public static int fill(Collection<String> collection, String str, int times){
+        String shrunk = shrink(str);
+        times = (times + shrunk.length()) / 2;
+        for (int i = 0; i < times / 2; i++) {
+            collection.add(shrunk);
+        }
+        return times;
+    }
+
+    public static String shrink(String str){
+        int newLength = str.length() / 2 + str.length() % 2;
+        char[] chars = new char[newLength];
+        for (int i = 0; i < str.length(); i+=2) {
+            chars[i / 2] = str.charAt(i);
+        }
+        return new String(chars);
+    }
+}
