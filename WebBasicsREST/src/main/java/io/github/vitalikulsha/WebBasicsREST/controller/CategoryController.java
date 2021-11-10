49935aba8c05aebb6ffca75521aba84be3399a07
vitalikulsha <kulsha_85@mail.ru>
2021-11-10 20:47:27 +0300

    update category

--- WebBasicsREST/src/main/java/io/github/vitalikulsha/WebBasicsREST/controller/CategoryController.java
+++ WebBasicsREST/src/main/java/io/github/vitalikulsha/WebBasicsREST/controller/CategoryController.java
@@ -38,6 +38,9 @@ public class CategoryController {
         }
     }
 
+    @GetMapping("/all")
+
+
     @DeleteMapping("/{id}")
     public ResponseEntity deleteCategory(@PathVariable Long id) {
         try {
